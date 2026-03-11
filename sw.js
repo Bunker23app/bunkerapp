467f007d1c6f5c225803eb57610b0ac8427b9b03
bunker23app <i.chiaro92@gmail.com>
2026-03-11 12:03:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('push', function(event) {
+  if (!event.data) return;
+
+  var data = event.data.json();
+  var title = data.title || 'BUNKER23';
+  var options = {
+    body: data.body || '',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [200, 100, 200],
+    data: { url: data.url || '/' }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
